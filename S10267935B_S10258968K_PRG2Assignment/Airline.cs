ed08e66a69e525bed121753edae1958f6993991f
Dylan2211 <167053963+Dylan2211@users.noreply.github.com>
2025-01-21 17:16:42 +0800

    Add files via upload
    trying to add my files that I've already done

---
+++ S10267935B_S10258968K_PRG2Assignment/Airline.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10267935B_S10258968K_PRG2Assignment
+{
+    class Airline
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public Dictionary<string, Flight> Flights{ get; set; }
+
+        public bool AddFlight(Flight f)
+        {
+
+        }
+        public double CalculateFees()
+        {
+            
+        }
+        public bool RemoveFlight(Flight f)
+        {
+
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ S10267935B_S10258968K_PRG2Assignment/Flight.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10267935B_S10258968K_PRG2Assignment
+{
+    class Flight
+    {
+        public string FlightNumber { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public DateTime ExpectedTime { get; set; }
+        public string Status { get; set; }
+
+        public double CalculateFees()
+        {
+
+        }
+        public
+    }
+}
