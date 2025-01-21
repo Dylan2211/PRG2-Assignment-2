e59f9e7b2c222cc07bbf9e7ca198e53a2c672289
Dylan2211 <167053963+Dylan2211@users.noreply.github.com>
2025-01-21 17:23:45 +0800

    Update Flight.cs
    Added Constructor

--- S10267935B_S10258968K_PRG2Assignment/Flight.cs
+++ S10267935B_S10258968K_PRG2Assignment/Flight.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,16 +8,25 @@ namespace S10267935B_S10258968K_PRG2Assignment
 {
     class Flight
     {
+        //Properties
         public string FlightNumber { get; set; }
         public string Origin { get; set; }
         public string Destination { get; set; }
         public DateTime ExpectedTime { get; set; }
         public string Status { get; set; }
-
+        //Methods
         public double CalculateFees()
         {
 
         }
-        public
+        //Constructor
+        public Flight(string flightNumber, string origin, string destination, DateTime expectedTime, string status)
+        {
+            FlightNumber = flightNumber;
+            Origin = origin;
+            Destination = destination;
+            ExpectedTime = expectedTime;
+            Status = status;
+        }
     }
 }
