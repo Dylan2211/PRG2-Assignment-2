0688afe649fe72f444aea50e4e119bd025cbc6a9
Dylan2211 <167053963+Dylan2211@users.noreply.github.com>
2025-01-21 17:18:01 +0800

    Update Program.cs

--- S10267935B_S10258968K_PRG2Assignment/Program.cs
+++ S10267935B_S10258968K_PRG2Assignment/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//1 Load Files
+Dictionary<string, string> AirlineDictionary = new Dictionary<string, string>();
+foreach (string line in File.ReadLines("airlines.csv").Skip(1))
+{
+    string[] splitLine = line.Split(",");
+    string Airline_Name = splitLine[0];
+    string Airline_Code = splitLine[1];
+    AirlineDictionary[Airline_Name] = Airline_Code;
+}
