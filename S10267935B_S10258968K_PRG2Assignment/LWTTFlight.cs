daefc8bec8757064e88851c59c2a7de84f9c7d14
Dylan <dylan.wongyk@gmail.com>
2025-01-24 11:44:57 +0800

    LWTT Flight and DDJB Flight

---
+++ S10267935B_S10258968K_PRG2Assignment/LWTTFlight.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10267935B_S10258968K_PRG2Assignment
+{
+    class LWTTFlight : Flight
+    {
+        public LWTTFlight(string flightNumber, string flightOrigin, string flightDestination, DateTime flightExpectedTime, string flightStatus) : base(flightNumber, flightOrigin, flightDestination, flightExpectedTime, flightStatus)
+        {
+        }
+        public double CalculateFees()
+        {
+            //double fees = 0;
+            //if (FlightStatus == "Landed" || FlightStatus == "Departed")
+            //{
+            //    fees = 0;
+            //}
+            //else if (FlightStatus == "Cancelled")
+            //{
+            //    fees = 0;
+            //}
+            //else if (FlightStatus == "Boarding")
+            //{
+            //    fees = 50;
+            //}
+            //else if (FlightStatus == "Check-In")
+            //{
+            //    fees = 100;
+            //}
+            //else if (FlightStatus == "Gate-Closed")
+            //{
+            //    fees = 150;
+            //}
+            //return fees;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
--- S10267935B_S10258968K_PRG2Assignment/obj/Debug/net8.0/S10267935B_S10258968K_PRG2Assignment.AssemblyInfo.cs
+++ S10267935B_S10258968K_PRG2Assignment/obj/Debug/net8.0/S10267935B_S10258968K_PRG2Assignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("S10267935B_S10258968K_PRG2Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c801ade66e8b8394f762738da4eb9cc4ad1fc8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+448605c1f17442bc8f8556127afeabba9815b75b")]
 [assembly: System.Reflection.AssemblyProductAttribute("S10267935B_S10258968K_PRG2Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("S10267935B_S10258968K_PRG2Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
