df3461e8d9535924e253868c2c2cb75bb77352bf
Dylan <dylan.wongyk@gmail.com>
2025-01-25 21:31:10 +0800

    Terminal labeling

--- S10267935B_S10258968K_PRG2Assignment/Terminal.cs
+++ S10267935B_S10258968K_PRG2Assignment/Terminal.cs
@@ -9,21 +9,13 @@ namespace S10267935B_S10258968K_PRG2Assignment
 {
     class Terminal
     {
+        //Properties
         public string TerminalName { get; set; }
         public Dictionary<string, Airline> Airlines { get; set; }
         public Dictionary<string, Flight> Flights { get; set; }
         public Dictionary<string, BoardingGate> BoardingGates { get; set; }
         public Dictionary<string, double> GateFees { get; set; }
-        public Terminal(string terminalName, Dictionary<string, Airline> airlines, Dictionary<string, Flight> flights, Dictionary<string, BoardingGate> boardingGates, Dictionary<string, double> gatefees)
-        {
-            TerminalName = terminalName;
-            Airlines = airlines;
-            Flights = flights;
-            BoardingGates = boardingGates;
-            GateFees = gatefees;
-        }
-
-
+        //Methods
         public bool AddFlight(string flightCode, Flight flight)
         {
             foreach (var existingFlight in Flights.Values)
@@ -38,6 +30,15 @@ namespace S10267935B_S10258968K_PRG2Assignment
             Flights.Add(flightCode, flight);
             Console.WriteLine($"Flight {flightCode} added successfully.");
             return true;
+        }
+        //Constructor
+        public Terminal(string terminalName, Dictionary<string, Airline> airlines, Dictionary<string, Flight> flights, Dictionary<string, BoardingGate> boardingGates, Dictionary<string, double> gatefees)
+        {
+            TerminalName = terminalName;
+            Airlines = airlines;
+            Flights = flights;
+            BoardingGates = boardingGates;
+            GateFees = gatefees;
         }
     }
 }
