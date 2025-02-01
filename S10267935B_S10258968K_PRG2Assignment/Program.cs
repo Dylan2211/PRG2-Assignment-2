ee45198ed217a00d8ac07c410cfc712a71fd6ef0
Dylan <dylan.wongyk@gmail.com>
2025-02-01 22:17:08 +0800

    DisplayFlightDetails

--- S10267935B_S10258968K_PRG2Assignment/Program.cs
+++ S10267935B_S10258968K_PRG2Assignment/Program.cs
@@ -212,65 +212,61 @@ void AssignBoardingGateToFlight()
 DisplayFlightDetails();
 void DisplayFlightDetails()
 {
-    while (true)
+    // List all airlines available
+    foreach (Airline a in AirlineList)
     {
-        // List all airlines available
+        Console.WriteLine(a);
+    }
+    try
+    {
+        // Prompt user for airline code
+        Console.WriteLine("Please enter the 2-Letter Airline code: ");
+        string airlineCode = Console.ReadLine();
+        // Retrieve airline object
+        Airline airline = null;
         foreach (Airline a in AirlineList)
         {
-            Console.WriteLine(a);
+            if (a.Code == airlineCode)
+            {
+                airline = a;
+                break;
+            }
         }
-        try
+        if (airline == null)
         {
-            // Prompt user for airline code
-            Console.WriteLine("Please enter the 2-Letter Airline code: ");
-            string airlineCode = Console.ReadLine();
-            // Retrieve airline object
-            Airline airline = null;
-            foreach (Airline a in AirlineList)
-            {
-                if (a.Code == airlineCode)
-                {
-                    airline = a;
-                    break;
-                }
-            }
-            if (airline == null)
-            {
-                Console.WriteLine("Error: Airline not found. Please try again.");
-                continue;
-            }
-            // Display Flight Airline Number, Origin, Destination
-            foreach (Flight f in airline.Flights.Values)
-            {
-                Console.WriteLine($"Flight Number: {f.FlightNumber,-7} Origin: {f.Origin,-15} Destination: {f.Destination,-15}");
-            }
-            // Prompt user for Flight Number
-            Console.WriteLine("Please enter the Flight Number: ");
-            string flightNumber = Console.ReadLine();
-            // Retrieve flight object
-            Flight flight = null;
-            foreach (Flight f in airline.Flights.Values)
-            {
-                if (f.FlightNumber == flightNumber)
-                {
-                    flight = f;
-                    break;
-                }
-            }
-            if (flight == null)
-            {
-                Console.WriteLine("Error: Flight not found. Please try again.");
-                continue;
-            }
-            // Display all flights from the airline
-            Console.WriteLine(flight + "Airline Name: " + airline.Name);
-            break;
+            Console.WriteLine("Error: Airline not found. Please try again.");
+            return;
         }
-        catch (Exception e)
+        // Display Flight Airline Number, Origin, Destination
+        foreach (Flight f in airline.Flights.Values)
         {
-            Console.WriteLine(e.Message);
-            continue;
+            Console.WriteLine($"Flight Number: {f.FlightNumber,-7} Origin: {f.Origin,-15} Destination: {f.Destination,-15}");
         }
+        // Prompt user for Flight Number
+        Console.WriteLine("Please enter the Flight Number: ");
+        string flightNumber = Console.ReadLine();
+        // Retrieve flight object
+        Flight flight = null;
+        foreach (Flight f in airline.Flights.Values)
+        {
+            if (f.FlightNumber == flightNumber)
+            {
+                flight = f;
+                break;
+            }
+        }
+        if (flight == null)
+        {
+            Console.WriteLine("Error: Flight not found. Please try again.");
+            return;
+        }
+        // Display all flights from the airline
+        Console.WriteLine(flight + "Airline Name: " + airline.Name);
+        return;
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("An error occurred: " + e.Message);
     }
 }
 
