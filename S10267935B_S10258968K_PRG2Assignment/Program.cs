5aa2fa266c60bc700c16870a6cdbee48b475237e
Dylan <dylan.wongyk@gmail.com>
2025-02-01 15:50:55 +0800

    Make parts of the code a function instead

--- S10267935B_S10258968K_PRG2Assignment/Program.cs
+++ S10267935B_S10258968K_PRG2Assignment/Program.cs
@@ -58,31 +58,39 @@ foreach (string line in File.ReadLines("flights.csv").Skip(1)) // Skipping heade
 }
 
 // 3. Print all flight details
-foreach (var entry in FlightDictionary)
+ListFlightDetails();
+void ListFlightDetails()
 {
-    Flight flight = entry.Value;
-    Console.WriteLine(flight);
-    //Console.WriteLine($"{flight.FlightNumber} {flight.Origin} {flight.Destination} {flight.ExpectedTime:dd/MM/yyyy hh:mm tt}");
+    foreach (var entry in FlightDictionary)
+    {
+        Flight flight = entry.Value;
+        Console.WriteLine(flight);
+        //Console.WriteLine($"{flight.FlightNumber} {flight.Origin} {flight.Destination} {flight.ExpectedTime:dd/MM/yyyy hh:mm tt}");
+    }
 }
 
 // 4. List all boarding gates
-string[] ddjb = { "A10", "A11", "A12", "A13", "A20", "A21", "A22", "B10", "B11", "B12" };
-string[] cfft = { "B1", "B2", "B3", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20", "C21", "C22" };
-string[] lwtt = { "A1", "A2", "A20", "A21", "A22", "C14", "C15", "C16", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "B11", "B12", "B13", "B14", "B15", "B16", "B17", "B18", "B19", "B20", "B21", "B22" };
-foreach (var entry in BoardingGate)
+ListAllBoardingGates();
+void ListAllBoardingGates()
 {
-    List<string> specialRequests = new List<string>();
-    BoardingGate gate = entry.Value;
+    string[] ddjb = { "A10", "A11", "A12", "A13", "A20", "A21", "A22", "B10", "B11", "B12" };
+    string[] cfft = { "B1", "B2", "B3", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20", "C21", "C22" };
+    string[] lwtt = { "A1", "A2", "A20", "A21", "A22", "C14", "C15", "C16", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "B11", "B12", "B13", "B14", "B15", "B16", "B17", "B18", "B19", "B20", "B21", "B22" };
+    foreach (var entry in BoardingGate)
+    {
+        List<string> specialRequests = new List<string>();
+        BoardingGate gate = entry.Value;
 
-    if (ddjb.Any(gatename => gate.GateName.Equals(gatename)))
-        specialRequests.Add("DDJB");
-    if (cfft.Any(gatename => gate.GateName.Equals(gatename)))
-        specialRequests.Add("CFFT");
-    if (lwtt.Any(gatename => gate.GateName.Equals(gatename)))
-        specialRequests.Add("LWTT");
+        if (ddjb.Any(gatename => gate.GateName.Equals(gatename)))
+            specialRequests.Add("DDJB");
+        if (cfft.Any(gatename => gate.GateName.Equals(gatename)))
+            specialRequests.Add("CFFT");
+        if (lwtt.Any(gatename => gate.GateName.Equals(gatename)))
+            specialRequests.Add("LWTT");
 
-    string formattedSpecialRequests = string.Join(", ", specialRequests);
-    Console.WriteLine($"{gate.GateName} {gate.SupportsCFFT} {gate.SupportsDDJB} {gate.SupportsLWTT} {formattedSpecialRequests}");
+        string formattedSpecialRequests = string.Join(", ", specialRequests);
+        Console.WriteLine($"{gate.GateName} {gate.SupportsCFFT} {gate.SupportsDDJB} {gate.SupportsLWTT} {formattedSpecialRequests}");
+    }
 }
 
 // 7. Display full flight details from an airline
@@ -150,3 +158,5 @@ void DisplayFlightDetails()
         }
     }
 }
+
+// 8. Modify flight details
